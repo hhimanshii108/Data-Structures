19f52881571e1bb745111d50fc0089536b47fa26
hhimanshii108 <100212269+hhimanshii108@users.noreply.github.com>
2022-08-06 19:53:21 +0530

    Add files via upload

---
+++ Sorting/Selection_Sort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+void selection(int arr[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        int min=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[min])
+                min=j;
+        }
+        swap(arr[min],arr[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    selection(arr,n);
+}
