931b6e4765d212918c008d99ad3788860c403c3d
hhimanshii108 <100212269+hhimanshii108@users.noreply.github.com>
2022-08-07 13:50:17 +0530

    Update KthSmallLarge.cpp

--- Array/KthSmallLarge.cpp
+++ Array/KthSmallLarge.cpp
@@ -1,4 +1,4 @@
-// BRUTE FORCE APPROACH
+// BRUTE FORCE APPROACH  TC- O(nlong)
 #include<algorithm>
 #include<iostream>
 #include<vector>
@@ -13,3 +13,32 @@ vector<int> kthSmallLarge(vector<int> &arr, int n, int k)
     v.push_back(max);
     return v;
 }
+
+// PRIORITY QUEUE       TC-O(nlogk)
+
+#include<bits/stdc++.h>
+#include<algorithm>
+#include<iostream>
+#include<vector>
+#include<stdlib.h>
+vector<int> kthSmallLarge(vector<int> &arr, int n, int k)
+{
+	vector<int>v;
+    priority_queue<int>maxh;
+    priority_queue<int>minh;
+    for(int i=0;i<n;i++)
+    {
+        maxh.push(arr[i]);
+        minh.push(-(arr[i]));
+    
+    if(maxh.size()>k)
+    {
+        maxh.pop();
+        minh.pop();
+    }
+    }
+    v.push_back(maxh.top());
+    v.push_back(-minh.top());
+    return v;
+}
+
