3f46957b1b10671610d7b3368bdb52c4c0946d00
hhimanshii108 <100212269+hhimanshii108@users.noreply.github.com>
2022-08-09 18:49:04 +0530

    Create CombinationSum3.cpp

---
+++ Recursion/CombinationSum3.cpp
@@ -0,0 +1,34 @@
+// when no duplicate ans are allowed
+// TC- O(2^n * k)
+// SC- k*x
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void occurence(int ind,int target,vector<int>& arr,vector<vector<int>>&ans, vector<int>ds)
+   {
+      if(target==0)
+      {
+          ans.push_back(ds);
+          return;
+      }
+        for(int i=ind;i<arr.size();i++)
+        {
+            if(i>ind && arr[i]==arr[i-1])
+                continue;
+            if(arr[i]>target)
+                break;
+            ds.push_back(arr[i]);
+            occurence(i+1,target-arr[i],arr,ans,ds);
+            ds.pop_back();
+        }
+   }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) 
+    {
+        sort(candidates.begin(), candidates.end());
+      vector<vector<int>>ans;
+        vector<int>ds;
+        occurence(0,target,candidates,ans,ds);
+        return ans;
+    }
+};
